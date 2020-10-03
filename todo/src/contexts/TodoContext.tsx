8e92e6e6557728ab4781aac21232cad6a09269df
Antonio Matos <acxmatos@gmail.com>
2020-10-02 23:15:33 -0300

    Mudancas Video 16

--- todo/src/contexts/TodoContext.tsx
+++ todo/src/contexts/TodoContext.tsx
@@ -1,9 +1,40 @@
 import React, { createContext } from 'react';
 import { TodoContextType } from './TodoContextType';
+import { Todo } from '../models/Todo';
 
 export const TodoContext = createContext<TodoContextType>({
   todos: [],
-  addTodo: () => { },
-  removeTodo: () => { },
-  toggle: () => { },
+  addTodo: () => {},
+  removeTodo: () => {},
+  toggle: () => {}
 });
+
+const TodoProvider = (props: any) => {
+  const todos: Todo[] = [
+    { id: 1, title: 'Ir ao supermercado', done: true },
+    { id: 2, title: 'Ir a academia', done: false }
+  ];
+  const addTodo = (title: string) => {
+    console.log('Adicionou', title);
+  };
+  const removeTodo = (todo: Todo) => {
+    console.log('Removeu', todo.title);
+  };
+  const toggle = (todo: Todo) => {
+    console.log('Alterou', todo);
+  };
+  return (
+    <TodoContext.Provider
+      value={{
+        todos,
+        addTodo,
+        removeTodo,
+        toggle
+      }}
+    >
+      {props.children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoProvider;
