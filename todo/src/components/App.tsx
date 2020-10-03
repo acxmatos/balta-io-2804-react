24c30132fbdd49c21f59d4e96830ccd93fb0af06
Antonio Matos <acxmatos@gmail.com>
2020-10-02 21:38:39 -0300

    Mudancas Video 9

--- todo/src/components/App.tsx
+++ todo/src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import TodoList from './TodoList';
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <TodoList></TodoList>
       <h1>Meu Primeiro App</h1>
     </div>
   );
